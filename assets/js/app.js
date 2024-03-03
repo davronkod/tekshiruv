24cfe266827f89bf70d89d9fcbd49cad9e46851d
Davronbek <davroncoder074@gmail.com>
2024-03-03 11:44:19 +0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,12 @@
 let yosh = 14;
 console.log(yosh);
+yosh = 17
+console.log(yosh);
+// let =>dasturimiz ishlashi davomida o'zgarishi mumkin bo'lgan o'zgaruvchilarni e'lon qilish uchun foydalaniladi.
+const id = 'o7';
+console.log(id);
+// const =>dasturimiz ishlashi davomida o'zgarmaydigan o'zgaruvchilarni e'lon qilish uchun foydalaniladi.
+// const yordamida e'lon qilingan o'zgaruvchilar har doim boshlang'ich qiymatga ega bo'lishi shart.
+
+
+// var =>let va var o'xshash bo'lishiga qaramasdan ularning ma'lum bir farqlari mavjud. var yordamida e'lon qilingan o'zgaruvchining ko'rinishi ko'lami (scope) funksiyasi darajasida bo'lsa, let esa blok darajada ko'rinadi.
